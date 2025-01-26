6ef397a0fe0c7018e710324e182994bc0e695fb6
David Kocher <dkocher@iterate.ch>
2025-01-27 00:05:48 +0100

    Do not log account key.

--- hub/src/main/java/ch/iterate/hub/workflows/UserKeysServiceImpl.java
+++ hub/src/main/java/ch/iterate/hub/workflows/UserKeysServiceImpl.java
@@ -119,7 +119,6 @@ public class UserKeysServiceImpl implements UserKeysService {
 
                 log.info("(3.1) generate and display new Account Key");
                 final String setupCode = prompt.generateAccountKey();
-                log.info("With setupCode={}", setupCode);
 
                 final String deviceName = prompt.displayAccountKeyAndAskDeviceName(host,
                         new AccountKeyAndDeviceName().withAccountKey(setupCode).withDeviceName(COMPUTER_NAME));
