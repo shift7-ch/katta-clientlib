8273407b4590b103e079a69dbf557f18597fb1c4
chenkins <chenkins44@gmail.com>
2025-01-28 20:14:00 +0100

    Improve test caching wot service.

--- hub/src/test/java/ch/iterate/hub/workflows/CachingWoTServiceTest.java
+++ hub/src/test/java/ch/iterate/hub/workflows/CachingWoTServiceTest.java
@@ -8,12 +8,15 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import java.text.ParseException;
+import java.util.HashMap;
 
 import ch.iterate.hub.client.ApiException;
+import ch.iterate.hub.client.model.TrustedUserDto;
 import ch.iterate.hub.workflows.exceptions.AccessException;
 import ch.iterate.hub.workflows.exceptions.SecurityFailure;
 import com.nimbusds.jose.JOSEException;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 
@@ -22,10 +25,15 @@ class CachingWoTServiceTest {
     @Test
     void testGetTrustLevelsPerUserId() throws AccessException, SecurityFailure, ApiException {
         final WoTService proxyMock = Mockito.mock(WoTService.class);
+        final HashMap<String, Integer> trustLevels = new HashMap<String, Integer>() {{
+            put("alkdajf", 5);
+            put("lakdjfa", 42);
+        }};
+        Mockito.when(proxyMock.getTrustLevelsPerUserId(any())).thenReturn(trustLevels);
         final CachingWoTService service = new CachingWoTService(proxyMock);
-        service.getTrustLevelsPerUserId(null);
+        assertEquals(trustLevels, service.getTrustLevelsPerUserId(null));
         Mockito.verify(proxyMock, times(1)).getTrustLevelsPerUserId(any());
-        service.getTrustLevelsPerUserId(null);
+        assertEquals(trustLevels, service.getTrustLevelsPerUserId(null));
         Mockito.verify(proxyMock, times(1)).getTrustLevelsPerUserId(any());
     }
 
@@ -42,10 +50,12 @@ class CachingWoTServiceTest {
     @Test
     void testSign() throws AccessException, SecurityFailure, ParseException, JOSEException, ApiException {
         final WoTService proxyMock = Mockito.mock(WoTService.class);
+        final TrustedUserDto trustedUser = new TrustedUserDto();
+        Mockito.when(proxyMock.sign(any(), any())).thenReturn(trustedUser);
         final CachingWoTService service = new CachingWoTService(proxyMock);
-        service.sign(null, null);
+        assertEquals(trustedUser, service.sign(null, null));
         Mockito.verify(proxyMock, times(1)).sign(any(), any());
-        service.sign(null, null);
+        assertEquals(trustedUser, service.sign(null, null));
         Mockito.verify(proxyMock, times(2)).sign(any(), any());
     }
 }
