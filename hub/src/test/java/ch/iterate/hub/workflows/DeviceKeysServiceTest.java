85dd159549a8503954f1b0231522dbe8d14a80c9
David Kocher <dkocher@iterate.ch>
2025-01-29 13:57:32 +0100

    Fix test.

--- hub/src/test/java/ch/iterate/hub/workflows/DeviceKeysServiceTest.java
+++ hub/src/test/java/ch/iterate/hub/workflows/DeviceKeysServiceTest.java
@@ -11,7 +11,7 @@ import java.security.spec.InvalidKeySpecException;
 import java.util.Base64;
 import java.util.UUID;
 
-import ch.iterate.hub.testsetup.HubTestUtilities;
+import ch.iterate.hub.testsetup.AbstractHubTest;
 
 import static ch.iterate.hub.workflows.DeviceKeysService.KEYCHAIN_PRIVATE_DEVICE_KEY_ACCOUNT_NAME;
 import static ch.iterate.hub.workflows.DeviceKeysService.KEYCHAIN_PUBLIC_DEVICE_KEY_ACCOUNT_NAME;
@@ -20,9 +20,9 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
-class DeviceKeysServiceTest {
+class DeviceKeysServiceTest extends AbstractHubTest {
     @Test
-    public void testGetDeviceKeysFromPasswordStore() throws InvalidKeySpecException, LocalAccessDeniedException {
+    void testGetDeviceKeysFromPasswordStore() throws InvalidKeySpecException, LocalAccessDeniedException {
         final PasswordStore storeMock = Mockito.mock(PasswordStore.class);
         final DeviceKeysService service = new DeviceKeysService(storeMock);
         final String userId = UUID.randomUUID().toString();
@@ -33,7 +33,7 @@ class DeviceKeysServiceTest {
     }
 
     @Test
-    public void testStoreDeviceKeysInPasswordStore() throws LocalAccessDeniedException {
+    void testStoreDeviceKeysInPasswordStore() throws LocalAccessDeniedException {
         final PasswordStore storeMock = Mockito.mock(PasswordStore.class);
         final DeviceKeysService service = new DeviceKeysService(storeMock);
         final String userId = UUID.randomUUID().toString();
@@ -45,9 +45,7 @@ class DeviceKeysServiceTest {
     }
 
     @Test
-    public void testStoreAndRetrieveDeviceKeys() throws InvalidKeySpecException, LocalAccessDeniedException {
-        HubTestUtilities.preferences();
-
+    void testStoreAndRetrieveDeviceKeys() throws InvalidKeySpecException, LocalAccessDeniedException {
         final String userId = UUID.randomUUID().toString();
         final String hubId = UUID.randomUUID().toString();
         assertNull(new DeviceKeysService().getDeviceKeysFromPasswordStore(userId, hubId));
