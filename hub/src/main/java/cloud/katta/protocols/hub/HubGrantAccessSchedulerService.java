67e7a133a3eeffed27322183d1e074348f6ad7c0
David Kocher <dkocher@iterate.ch>
2025-06-17 12:52:39 +0200

    Fix initialization.

--- hub/src/main/java/cloud/katta/protocols/hub/HubGrantAccessSchedulerService.java
+++ hub/src/main/java/cloud/katta/protocols/hub/HubGrantAccessSchedulerService.java
@@ -11,6 +11,8 @@ import ch.cyberduck.core.exception.BackgroundException;
 import ch.cyberduck.core.preferences.HostPreferences;
 import ch.cyberduck.core.shared.ThreadPoolSchedulerFeature;
 
+import cloud.katta.client.api.StorageProfileResourceApi;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -25,7 +27,6 @@ import cloud.katta.client.model.VaultDto;
 import cloud.katta.crypto.UserKeys;
 import cloud.katta.protocols.hub.exceptions.HubExceptionMappingService;
 import cloud.katta.workflows.DeviceKeysServiceImpl;
-import cloud.katta.workflows.GrantAccessService;
 import cloud.katta.workflows.GrantAccessServiceImpl;
 import cloud.katta.workflows.UserKeysServiceImpl;
 import cloud.katta.workflows.exceptions.AccessException;
@@ -36,40 +37,32 @@ public class HubGrantAccessSchedulerService extends ThreadPoolSchedulerFeature<H
 
     private final HubSession session;
     private final HostPasswordStore keychain;
-    private final VaultResourceApi vaults;
-    private final UsersResourceApi users;
-    private final DeviceResourceApi devices;
-    private final GrantAccessService service;
 
     public HubGrantAccessSchedulerService(final HubSession session, final HostPasswordStore keychain) {
-        this(session, keychain, new VaultResourceApi(session.getClient()), new UsersResourceApi(session.getClient()), new DeviceResourceApi(session.getClient()), new GrantAccessServiceImpl(session));
-    }
-
-    public HubGrantAccessSchedulerService(final HubSession session, final HostPasswordStore keychain, final VaultResourceApi vaults, final UsersResourceApi users, final DeviceResourceApi devices, final GrantAccessService service) {
         super(new HostPreferences(session.getHost()).getLong("hub.protocol.scheduler.period"));
         this.session = session;
         this.keychain = keychain;
-        this.vaults = vaults;
-        this.users = users;
-        this.devices = devices;
-        this.service = service;
     }
 
     @Override
     public Host operate(final PasswordCallback callback) throws BackgroundException {
         log.info("Scheduler for {}", session.getHost());
         try {
-            final UserKeys userKeys = new UserKeysServiceImpl(users, devices).getUserKeys(session.getHost(), session.getMe(),
+            final UserKeys userKeys = new UserKeysServiceImpl(
+                    new UsersResourceApi(session.getClient()),
+                    new DeviceResourceApi(session.getClient())).getUserKeys(session.getHost(), session.getMe(),
                     new DeviceKeysServiceImpl(keychain).getDeviceKeys(session.getHost()));
-
-            final List<VaultDto> accessibleVaults = vaults.apiVaultsAccessibleGet(Role.OWNER);
-
+            final List<VaultDto> accessibleVaults = new VaultResourceApi(session.getClient()).apiVaultsAccessibleGet(Role.OWNER);
             for(final VaultDto accessibleVault : accessibleVaults) {
                 if(Boolean.TRUE.equals(accessibleVault.getArchived())) {
                     log.debug("Skip archived vault {}", accessibleVault);
                     continue;
                 }
-                service.grantAccessToUsersRequiringAccessGrant(accessibleVault.getId(), userKeys);
+                new GrantAccessServiceImpl(
+                        new VaultResourceApi(session.getClient()),
+                        new StorageProfileResourceApi(session.getClient()),
+                        new UsersResourceApi(session.getClient())
+                ).grantAccessToUsersRequiringAccessGrant(accessibleVault.getId(), userKeys);
             }
             userKeys.destroy();
         }
