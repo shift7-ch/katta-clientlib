bdfe8b9fad18543423b1dca9f67581bedaea0a9b
chenkins <chenkins44@gmail.com>
2025-07-12 11:01:38 +0200

    Add storage profile archive test.

--- admin-cli/src/test/java/cloud/katta/cli/commands/hub/StorageProfileArchiveTest.java
+++ admin-cli/src/test/java/cloud/katta/cli/commands/hub/StorageProfileArchiveTest.java
@@ -4,80 +4,26 @@
 
 package cloud.katta.cli.commands.hub;
 
-
-import ch.cyberduck.core.PasswordStoreFactory;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.testcontainers.containers.ComposeContainer;
-import org.testcontainers.containers.wait.strategy.LogMessageWaitStrategy;
+import org.mockito.Mockito;
 
-import java.io.File;
-import java.io.IOException;
-import java.net.URISyntaxException;
-import java.time.Duration;
-import java.util.HashMap;
-import java.util.Properties;
-import java.util.stream.Collectors;
+import java.util.UUID;
 
-import cloud.katta.protocols.hub.HubSession;
-import cloud.katta.testsetup.AbstractHubTest;
-import cloud.katta.testsetup.HubTestConfig;
-import cloud.katta.testsetup.HubTestSetupDockerExtension;
+import cloud.katta.client.ApiException;
+import cloud.katta.client.api.StorageProfileResourceApi;
 
-class StorageProfileArchiveTest extends AbstractHubTest {
-    private static final Logger log = LogManager.getLogger(StorageProfileArchiveTest.class.getName());
-    public static ComposeContainer compose;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
 
+class StorageProfileArchiveTest {
 
     @Test
-    public void testHubWorkflow() throws Exception {
-        HubTestConfig.Setup config = new HubTestConfig.Setup().withUserConfig(
-                new HubTestConfig.Setup.UserConfig("admin", "admin", "setupcode")
-        ).withHubURL("http://localhost:8280");
-        final HubSession hubSession = setupConnection(config);
-        final String accessToken = PasswordStoreFactory.get().findOAuthTokens(hubSession.getHost()).getAccessToken();
-
-        new StorageProfileArchive().call("http://localhost:8280", accessToken, "732D43FA-3716-46C4-B931-66EA5405EF1C");
+    public void testCall() throws ApiException {
+        final StorageProfileResourceApi proxyMock = Mockito.mock(StorageProfileResourceApi.class);
+        final UUID vaultId = UUID.randomUUID();
+        new StorageProfileArchive().call(vaultId.toString(), proxyMock);
+        Mockito.verify(proxyMock, times(1)).apiStorageprofileProfileIdPut(vaultId, true);
+        Mockito.verify(proxyMock, times(1)).apiStorageprofileProfileIdPut(any(), any());
     }
 
-    @BeforeAll
-    public static void setupDocker() throws URISyntaxException, IOException {
-        final HubTestConfig.Setup.DockerConfig configuration = new HubTestConfig.Setup.DockerConfig("/docker-compose-minio-localhost-hub.yml", "/.local.env", "local", "admin", "admin", "top-secret");
-        log.info("Setup docker {}", configuration);
-        final Properties props = new Properties();
-        props.load(StorageProfileArchiveTest.class.getResourceAsStream(configuration.envFile));
-        final HashMap<String, String> env = props.entrySet().stream().collect(
-                Collectors.toMap(
-                        e -> String.valueOf(e.getKey()),
-                        e -> String.valueOf(e.getValue()),
-                        (prev, next) -> next, HashMap::new
-                ));
-        env.put("HUB_ADMIN_USER", configuration.hubAdminUser);
-        env.put("HUB_ADMIN_PASSWORD", configuration.hubAdminPassword);
-        env.put("HUB_KEYCLOAK_SYSTEM_CLIENT_SECRET", configuration.hubKeycloakSystemClientSecret);
-        compose = new ComposeContainer(
-                new File(HubTestSetupDockerExtension.class.getResource(configuration.composeFile).toURI()))
-                .withLocalCompose(true)
-                .withPull(true)
-                .withEnv(env)
-                .withOptions(configuration.profile == null ? "" : String.format("--profile=%s", configuration.profile))
-                .waitingFor("wait-1", new LogMessageWaitStrategy().withRegEx(".*exit 0.*").withStartupTimeout(Duration.ofMinutes(2)));
-        compose.start();
-
-        log.info("Done setup docker {}", configuration);
-    }
-
-    @AfterAll
-    public static void teardownDocker() {
-        try {
-            compose.stop();
-        }
-        catch(Exception e) {
-            log.warn(e);
-        }
-    }
 }
