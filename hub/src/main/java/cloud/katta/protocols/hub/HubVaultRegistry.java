76d08f3f682d2977a025f4f4cf47760e15aabed8
David Kocher <dkocher@iterate.ch>
2025-08-26 12:25:57 +0200

    Logging.

--- hub/src/main/java/cloud/katta/protocols/hub/HubVaultRegistry.java
+++ hub/src/main/java/cloud/katta/protocols/hub/HubVaultRegistry.java
@@ -12,8 +12,11 @@ import ch.cyberduck.core.features.Vault;
 import ch.cyberduck.core.vault.DefaultVaultRegistry;
 
 import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class HubVaultRegistry extends DefaultVaultRegistry {
+    private static final Logger log = LogManager.getLogger(HubSession.class);
 
     public HubVaultRegistry() {
         super(new DisabledPasswordCallback());
@@ -25,9 +28,11 @@ public class HubVaultRegistry extends DefaultVaultRegistry {
             if(StringUtils.equals(new DefaultPathContainerService().getContainer(file).getName(),
                     new DefaultPathContainerService().getContainer(vault.getHome()).getName())) {
                 // Return matching vault
+                log.debug("Found vault {} for file {}", vault, file);
                 return vault;
             }
         }
+        log.warn("No vault found for file {}", file);
         return Vault.DISABLED;
     }
 
