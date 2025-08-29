f958e82801dadbcfa405c9286a6c96df6186b227
David Kocher <dkocher@iterate.ch>
2025-08-29 21:42:10 +0200

    Remove logger.

--- hub/src/main/java/cloud/katta/protocols/hub/HubVaultRegistry.java
+++ hub/src/main/java/cloud/katta/protocols/hub/HubVaultRegistry.java
@@ -8,18 +8,14 @@ import ch.cyberduck.core.DisabledPasswordCallback;
 import ch.cyberduck.core.Session;
 import ch.cyberduck.core.vault.DefaultVaultRegistry;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 public class HubVaultRegistry extends DefaultVaultRegistry {
-    private static final Logger log = LogManager.getLogger(HubVaultRegistry.class);
 
     public HubVaultRegistry() {
         super(new DisabledPasswordCallback());
     }
 
     @Override
-    public <T> T getFeature(Session<?> session, Class<T> type, T proxy) {
+    public <T> T getFeature(final Session<?> session, final Class<T> type, final T proxy) {
         // Always forward to load feature from vault
         return this._getFeature(session, type, proxy);
     }
