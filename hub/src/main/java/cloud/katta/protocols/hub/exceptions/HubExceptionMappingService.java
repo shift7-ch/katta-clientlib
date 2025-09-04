f4770435f4eed07f80403f64134c0018a2d935c2
David Kocher <dkocher@iterate.ch>
2025-09-04 11:36:07 +0200

    Log failures.

--- hub/src/main/java/cloud/katta/protocols/hub/exceptions/HubExceptionMappingService.java
+++ hub/src/main/java/cloud/katta/protocols/hub/exceptions/HubExceptionMappingService.java
@@ -32,6 +32,7 @@ public class HubExceptionMappingService extends AbstractExceptionMappingService<
 
     @Override
     public BackgroundException map(final ApiException failure) {
+        log.warn("Map failure {}", failure.toString());
         for(final Throwable cause : ExceptionUtils.getThrowableList(failure)) {
             if(cause instanceof SocketException) {
                 // Map Connection has been shutdown: javax.net.ssl.SSLException: java.net.SocketException: Broken pipe
