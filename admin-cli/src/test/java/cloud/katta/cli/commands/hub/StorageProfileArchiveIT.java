69d9c9f24611a43b00e388299c6c415d536110de
chenkins <chenkins44@gmail.com>
2025-09-24 15:20:10 +0200

    Add assertions.

--- admin-cli/src/test/java/cloud/katta/cli/commands/hub/StorageProfileArchiveIT.java
+++ admin-cli/src/test/java/cloud/katta/cli/commands/hub/StorageProfileArchiveIT.java
@@ -7,13 +7,22 @@ package cloud.katta.cli.commands.hub;
 
 import ch.cyberduck.core.PasswordStoreFactory;
 
+import cloud.katta.client.model.StorageProfileS3Dto;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.ComposeContainer;
 
+import cloud.katta.client.api.StorageProfileResourceApi;
 import cloud.katta.protocols.hub.HubSession;
 import cloud.katta.testsetup.HubTestConfig;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+import java.util.Optional;
 
 class StorageProfileArchiveIT extends AbtractAdminCliIT {
     private static final Logger log = LogManager.getLogger(StorageProfileArchiveIT.class.getName());
@@ -21,13 +30,26 @@ class StorageProfileArchiveIT extends AbtractAdminCliIT {
 
 
     @Test
-    public void testHubWorkflow() throws Exception {
+    public void testStorageProfileArchive() throws Exception {
         HubTestConfig.Setup config = new HubTestConfig.Setup().withUserConfig(
                 new HubTestConfig.Setup.UserConfig("admin", "admin", "setupcode")
         ).withHubURL("http://localhost:8280");
         final HubSession hubSession = setupConnection(config);
         final String accessToken = PasswordStoreFactory.get().findOAuthTokens(hubSession.getHost()).getAccessToken();
 
-        new StorageProfileArchive().call("http://localhost:8280", accessToken, "732D43FA-3716-46C4-B931-66EA5405EF1C");
+
+        final String storageProfileId = "732D43FA-3716-46C4-B931-66EA5405EF1C".toLowerCase();
+        final StorageProfileResourceApi storageProfileResourceApi = new StorageProfileResourceApi(hubSession.getClient());
+        {
+            final Optional<StorageProfileS3Dto> profile = storageProfileResourceApi.apiStorageprofileS3Get().stream().filter(p -> p.getId().toString().toLowerCase().equals(storageProfileId)).findFirst();
+            assertTrue(profile.isPresent());
+            assertFalse(profile.get().getArchived());
+        }
+        new StorageProfileArchive().call("http://localhost:8280", accessToken, storageProfileId);
+        {
+            final Optional<StorageProfileS3Dto> profile = storageProfileResourceApi.apiStorageprofileS3Get().stream().filter(p -> p.getId().toString().toLowerCase().equals(storageProfileId)).findFirst();
+            assertTrue(profile.isPresent());
+            assertTrue(profile.get().getArchived());
+        }
     }
 }
