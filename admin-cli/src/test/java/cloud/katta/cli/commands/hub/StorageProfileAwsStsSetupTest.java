eab3627483762a85ffc1f469f9b649048702a27b
chenkins <chenkins44@gmail.com>
2025-09-25 09:29:45 +0200

    Cleanup package structure.

--- admin-cli/src/test/java/cloud/katta/cli/commands/hub/StorageProfileAwsStsSetupTest.java
+++ admin-cli/src/test/java/cloud/katta/cli/commands/hub/StorageProfileAwsStsSetupTest.java
@@ -2,7 +2,7 @@
  * Copyright (c) 2025 shift7 GmbH. All rights reserved.
  */
 
-package cloud.katta.cli.commands.setup;
+package cloud.katta.cli.commands.hub;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
