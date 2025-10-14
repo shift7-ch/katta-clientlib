4fafb54839198a65b88f431db60e5859118dd1b5
David Kocher <dkocher@iterate.ch>
2025-10-14 17:01:14 +0200

    Remove unused.

--- hub/src/main/java/cloud/katta/protocols/s3/S3AssumeRoleProtocol.java
+++ hub/src/main/java/cloud/katta/protocols/s3/S3AssumeRoleProtocol.java
@@ -20,7 +20,6 @@ public class S3AssumeRoleProtocol extends S3Protocol {
 
     // STS assume role with web identity from Cyberduck core (AWS + MinIO)
     public static final String S3_ASSUMEROLE_ROLEARN = Profile.STS_ROLE_ARN_PROPERTY_KEY;
-    public static final String S3_ASSUMEROLE_ROLESESSIONNAME = Profile.STS_ROLE_SESSION_NAME_PROPERTY_KEY;
     public static final String S3_ASSUMEROLE_DURATIONSECONDS = Profile.STS_DURATION_SECONDS_PROPERTY_KEY;
     // STS role chaining (AWS only)
     public static final String S3_ASSUMEROLE_ROLEARN_2 = "s3.assumerole.rolearn.2";
