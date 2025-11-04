a0c8799b385bcc612b51573b3015b5e952c407fe
chenkins <chenkins44@gmail.com>
2025-11-04 21:09:51 +0100

    Get rid of dependency to cloud.katta.testsetup apart from docker compose file, no need for HubSession in admin cli tests any more.

--- admin-cli/src/test/java/cloud/katta/testcontainers/AbtractAdminCliIT.java
+++ admin-cli/src/test/java/cloud/katta/testcontainers/AbtractAdminCliIT.java
@@ -14,55 +14,80 @@ import org.testcontainers.containers.wait.strategy.LogMessageWaitStrategy;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.time.Duration;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 import java.util.stream.Collectors;
 
-import cloud.katta.protocols.hub.HubSession;
-import cloud.katta.testsetup.AbstractHubTest;
-import cloud.katta.testsetup.HubTestConfig;
+import cloud.katta.client.ApiClient;
+import cloud.katta.client.ApiException;
+import cloud.katta.client.Pair;
+import cloud.katta.client.auth.HttpBearerAuth;
 
-public class AbtractAdminCliIT extends AbstractHubTest {
+import static io.restassured.RestAssured.given;
+
+public class AbtractAdminCliIT {
     private static final Logger log = LogManager.getLogger(AbtractAdminCliIT.class.getName());
     public static ComposeContainer compose;
 
-    protected HubSession hubSession;
+    protected String accessToken;
+    protected ApiClient apiClient;
 
     @BeforeAll
     public static void setupDocker() throws URISyntaxException, IOException {
-        final HubTestConfig.Setup.DockerConfig configuration = new HubTestConfig.Setup.DockerConfig("/docker-compose-minio-localhost-hub.yml", "/.local.env", "local", "admin", "admin", "top-secret");
-        log.info("Setup docker {}", configuration);
+        final String composeFile = "/docker-compose-minio-localhost-hub.yml";
+        final String envFile = "/.local.env";
+        final String profile = "local";
+        final String hubAdminUser = "admin";
+        final String hubAdminPassword = "admin";
+        final String hubKeycloakSystemClientSecret = "top-secret";
         final Properties props = new Properties();
-        props.load(AbtractAdminCliIT.class.getResourceAsStream(configuration.envFile));
+        props.load(AbtractAdminCliIT.class.getResourceAsStream(envFile));
         final HashMap<String, String> env = props.entrySet().stream().collect(
                 Collectors.toMap(
                         e -> String.valueOf(e.getKey()),
                         e -> String.valueOf(e.getValue()),
                         (prev, next) -> next, HashMap::new
                 ));
-        env.put("HUB_ADMIN_USER", configuration.hubAdminUser);
-        env.put("HUB_ADMIN_PASSWORD", configuration.hubAdminPassword);
-        env.put("HUB_KEYCLOAK_SYSTEM_CLIENT_SECRET", configuration.hubKeycloakSystemClientSecret);
+        env.put("HUB_ADMIN_USER", hubAdminUser);
+        env.put("HUB_ADMIN_PASSWORD", hubAdminPassword);
+        env.put("HUB_KEYCLOAK_SYSTEM_CLIENT_SECRET", hubKeycloakSystemClientSecret);
         compose = new ComposeContainer(
-                new File(AbtractAdminCliIT.class.getResource(configuration.composeFile).toURI()))
+                new File(AbtractAdminCliIT.class.getResource(composeFile).toURI()))
                 .withLocalCompose(true)
                 .withPull(true)
                 .withEnv(env)
-                .withOptions(configuration.profile == null ? "" : String.format("--profile=%s", configuration.profile))
+                .withOptions(profile == null ? "" : String.format("--profile=%s", profile))
                 .waitingFor("minio_setup-1", new LogMessageWaitStrategy().withRegEx(".*Completed MinIO Setup.*").withStartupTimeout(Duration.ofMinutes(2)));
         compose.start();
-
-        log.info("Done setup docker {}", configuration);
     }
 
     @BeforeEach
     protected void setup() throws Exception {
-        final HubTestConfig.Setup config = new HubTestConfig.Setup().withUserConfig(
-                new HubTestConfig.Setup.UserConfig("admin", "admin", "setupcode")
-        ).withHubURL("http://localhost:8280");
-        hubSession = setupConnection(config);
+        accessToken = given()
+                .header("Content-Type", "application/x-www-form-urlencoded")
+                .formParam("client_id", "cryptomator")
+                .formParam("grant_type", "password")
+                .formParam("username", "admin")
+                .formParam("password", "admin")
+                .when()
+                .post("http://localhost:8380/realms/cryptomator/protocol/openid-connect/token")
+                .then()
+                .statusCode(200)
+                .extract().path("access_token");
+        final HttpBearerAuth auth = new HttpBearerAuth("Bearer");
+        auth.setBearerToken(accessToken);
+        apiClient = new ApiClient() {
+            protected void updateParamsForAuth(String[] authNames, List<Pair> queryParams, Map<String, String> headerParams,
+                                               Map<String, String> cookieParams, String payload, String method, URI uri) throws ApiException {
+                auth.applyToParams(queryParams, headerParams, cookieParams, payload, method, uri);
+            }
+        };
+        apiClient.setBasePath("http://localhost:8280");
     }
 
     @AfterAll
