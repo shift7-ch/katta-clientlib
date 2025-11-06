acecf60cc6158e799ace65928b126580a9dec4c4
chenkins <chenkins44@gmail.com>
2025-11-06 13:45:04 +0100

    Add --region and --regions option for AWS STS setup.

--- admin-cli/src/test/java/cloud/katta/cli/commands/hub/StorageProfileAwsStaticSetupTest.java
+++ admin-cli/src/test/java/cloud/katta/cli/commands/hub/StorageProfileAwsStaticSetupTest.java
@@ -7,11 +7,13 @@ package cloud.katta.cli.commands.hub;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
+import java.util.List;
 import java.util.UUID;
 
 import cloud.katta.client.ApiException;
 import cloud.katta.client.api.StorageProfileResourceApi;
 import cloud.katta.client.model.Protocol;
+import cloud.katta.client.model.S3SERVERSIDEENCRYPTION;
 import cloud.katta.client.model.S3STORAGECLASSES;
 import cloud.katta.client.model.StorageProfileS3StaticDto;
 
@@ -24,17 +26,26 @@ class StorageProfileAwsStaticSetupTest {
         final StorageProfileResourceApi api = Mockito.mock(StorageProfileResourceApi.class);
         final UUID vaultId = UUID.randomUUID();
         final StorageProfileAwsStaticSetup cli = new StorageProfileAwsStaticSetup();
+
+        cli.region = "us-east-1";
+        cli.regions = List.of();
         cli.call(vaultId, "AWS S3 static", api);
+
 
         final StorageProfileS3StaticDto dto = new StorageProfileS3StaticDto();
         dto.setId(vaultId);
         dto.setName("AWS S3 static");
+        dto.setRegion("us-east-1");
         dto.setProtocol(Protocol.S3_STATIC);
         dto.setArchived(false);
         dto.setScheme("https");
         dto.setPort(443);
         dto.setWithPathStyleAccessEnabled(false);
+        dto.setBucketPrefix("katta-");
         dto.setStorageClass(S3STORAGECLASSES.STANDARD);
+        dto.setBucketEncryption(S3SERVERSIDEENCRYPTION.NONE);
+        dto.stsRoleCreateBucketClient("");
+        dto.stsRoleCreateBucketHub("");
         Mockito.verify(api, times(1)).apiStorageprofileS3staticPost(dto);
         Mockito.verify(api, times(1)).apiStorageprofileS3staticPost(any());
     }
