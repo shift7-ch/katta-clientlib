77859f373d93646809bb20c8e7e9db6489d70ffd
David Kocher <dkocher@iterate.ch>
2026-03-05 15:55:30 +0100

    Review defaults. Name with -role suffix.

--- admin-cli/src/main/java/cloud/katta/cli/commands/common/Defaults.java
+++ admin-cli/src/main/java/cloud/katta/cli/commands/common/Defaults.java
@@ -9,8 +9,8 @@ public class Defaults {
     public static final String CREATE_BUCKET_ROLE_NAME_INFIX = "create-bucket";
     public static final String ACCESS_BUCKET_ROLE_NAME_INFIX = "access-bucket";
 
-    public static final String ASSUME_ROLE_WITH_WEB_IDENTITY_ROLE_SUFFIX = "-a-role-web-identity";
-    public static final String ASSUME_ROLE_TAGGED_SESSION_ROLE_SUFFIX = "-a-role-tagged-session";
+    public static final String ASSUME_ROLE_WITH_WEB_IDENTITY_ROLE_SUFFIX = "-web-identity-role";
+    public static final String ASSUME_ROLE_TAGGED_SESSION_ROLE_SUFFIX = "-tagged-session-role";
 
     public static final String REQUEST_TAG = "Vault";
 }
