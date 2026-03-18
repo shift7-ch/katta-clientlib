9d313ec5928b3b36388f9210d1c83ef4b6606a73
David Kocher <dkocher@iterate.ch>
2026-03-18 20:34:35 +0100

    Fix test.

--- admin-cli/src/test/java/cloud/katta/cli/commands/storage/aws/AWSSTSStorageTest.java
+++ admin-cli/src/test/java/cloud/katta/cli/commands/storage/aws/AWSSTSStorageTest.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import software.amazon.awssdk.services.iam.IamClient;
+import software.amazon.awssdk.services.iam.model.GetOpenIdConnectProviderRequest;
+import software.amazon.awssdk.services.iam.model.GetOpenIdConnectProviderResponse;
 import software.amazon.awssdk.services.iam.model.GetRoleRequest;
 import software.amazon.awssdk.services.iam.model.GetRoleResponse;
 import software.amazon.awssdk.services.iam.model.ListOpenIdConnectProvidersResponse;
@@ -22,8 +24,13 @@ class AWSSTSStorageTest {
     @Test
     public void testAwsSetup() throws Exception {
         final IamClient iam = Mockito.mock(IamClient.class);
-        Mockito.when(iam.listOpenIDConnectProviders()).thenReturn(ListOpenIdConnectProvidersResponse.builder().openIDConnectProviderList(OpenIDConnectProviderListEntry.builder().arn("arnP").build()).build());
-        Mockito.when(iam.getRole(GetRoleRequest.builder().roleName("prae-access-bucket-web-identity-role").build())).thenReturn(GetRoleResponse.builder().role(Role.builder().arn("arn").build()).build());
+        Mockito.when(iam.getOpenIDConnectProvider(GetOpenIdConnectProviderRequest.builder()
+                        .openIDConnectProviderArn("arnP").build()))
+                .thenReturn(GetOpenIdConnectProviderResponse.builder().build());
+        Mockito.when(iam.listOpenIDConnectProviders())
+                .thenReturn(ListOpenIdConnectProvidersResponse.builder().openIDConnectProviderList(OpenIDConnectProviderListEntry.builder().arn("arnP").build()).build());
+        Mockito.when(iam.getRole(GetRoleRequest.builder().roleName("prae-access-bucket-web-identity-role").build()))
+                .thenReturn(GetRoleResponse.builder().role(Role.builder().arn("arn").build()).build());
         final AWSSTSStorage cli = new AWSSTSStorage();
         cli.bucketPrefix = "prefix-";
         cli.roleNamePrefix = "prae-";
