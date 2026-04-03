e20405a119bd52d049c548e1f4131bf63f8f3843
chenkins <chenkins44@gmail.com>
2026-04-03 11:51:23 +0200

    Add admin cli example for local dev setup.

---
+++ admin-cli/src/test/java/cloud/katta/examples/Local.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2026 shift7 GmbH. All rights reserved.
+ */
+
+package cloud.katta.examples;
+
+import cloud.katta.cli.Katta;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import picocli.CommandLine;
+
+import java.util.UUID;
+
+/**
+ * Example to use with
+ * <code>docker compose -f test/src/test/resources/docker-compose-minio-localhost-hub.yml --profile local --profile demo up --wait</code>.
+ */
+public class Local {
+    public static void main(String[] args) {
+        final String bucketPrefix = "katta-test-";
+        final String realmUrl = String.format("http://localhost:8180/realms/cryptomator");
+        final String hubUrl = String.format("http://localhost:8080");
+        final String region = "eu-central-1";
+        final String tokenUrl = String.format("%s/protocol/openid-connect/token", realmUrl);
+        final String authUrl = String.format("%s/protocol/openid-connect/auth", realmUrl);
+        if(false) {
+            final UUID storageProfileId = UUID.randomUUID();
+            final String[] storageProfileMinIOStsArgs = {
+                    "storageprofile", "minio", "sts",
+                    "--tokenUrl", tokenUrl,
+                    "--authUrl", authUrl,
+                    "--clientId", "cryptomator",
+                    "--hubUrl", hubUrl,
+                    "--uuid", storageProfileId.toString(),
+                    "--name", "MinIO S3 STS",
+                    "--bucketPrefix", bucketPrefix,
+                    "--endpointUrl", "http://localhost:9000",
+                    "--stsRoleAccessBucket", "arn:minio:iam:::role/Hdms6XDZ6oOpuWYI3gu4gmgHN94", // cryptomatorvaults
+                    // TODO works onyl in Desktop xor Web - differentiate:
+                    "--stsRoleCreateBucket", "arn:minio:iam:::role/HGKdlY4eFFsXVvJmwlMYMhmbnDE",
+                    //"--stsRoleCreateBucketClient", "arn:minio:iam:::role/IqZpDC5ahW_DCAvZPZA4ACjEnDE", // cryptomator (Desktop)
+                    //"--stsRoleCreateBucketHub", "arn:minio:iam:::role/HGKdlY4eFFsXVvJmwlMYMhmbnDE", // cryptomatorhub (Web)
+                    "--region", region,
+                    "--regions", region
+            };
+            System.out.println(String.format("katta \"%s\"", String.join("\" \"", storageProfileMinIOStsArgs)));
+            int rc = new CommandLine(new Katta()).execute(
+                    storageProfileMinIOStsArgs
+            );
+            assertEquals(0, rc);
+        }
+        if(true) {
+            final UUID storageProfileId = UUID.randomUUID();
+            final String[] storageProfileAwsStaticArgs = {
+                    "storageprofile", "s3", "static",
+                    "--tokenUrl", tokenUrl,
+                    "--authUrl", authUrl,
+                    "--clientId", "cryptomator",
+                    "--hubUrl", hubUrl,
+                    "--uuid", storageProfileId.toString(),
+                    "--name", "MinIO S3 Static",
+                    "--endpointUrl", "http://localhost:9000",
+                    "--region", "eu-west-1",
+                    "--regions", "eu-west-1",
+                    "--regions", "eu-west-2",
+                    "--regions", "eu-west-3"};
+            System.out.println(String.format("katta \"%s\"", String.join("\" \"", storageProfileAwsStaticArgs)));
+            int rc = new CommandLine(new Katta()).execute(
+                    storageProfileAwsStaticArgs
+            );
+            assertEquals(0, rc);
+        }
+    }
+}
