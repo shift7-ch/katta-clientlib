bcf3f62d87df16db5d4cdce8b9849a8b3f94e37a
chenkins <chenkins44@gmail.com>
2026-05-29 16:04:42 +0200

    Import new openapi.json from katta-server corresponding branch.

--- hub/src/main/java/cloud/katta/protocols/hub/HubStorageLocationService.java
+++ hub/src/main/java/cloud/katta/protocols/hub/HubStorageLocationService.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2025 shift7 GmbH. All rights reserved.
+ * Copyright (c) 2026 shift7 GmbH. All rights reserved.
  */
 
 package cloud.katta.protocols.hub;
@@ -19,6 +19,7 @@ import java.util.Set;
 
 import cloud.katta.client.ApiException;
 import cloud.katta.client.api.StorageProfileResourceApi;
+import cloud.katta.client.model.SettingsDto;
 import cloud.katta.client.model.StorageProfileDto;
 import cloud.katta.crypto.uvf.UVFMetadataPayload;
 import cloud.katta.crypto.uvf.VaultMetadataAutomaticAccessGrantDto;
@@ -120,16 +121,17 @@ public class HubStorageLocationService implements Location {
             return new StorageLocation(storage.getProvider(), storage.getRegion(), storage.getNickname());
         }
 
-        public UVFMetadataPayload toPayload(final Path bucket) {
-            return this.toPayload(bucket, new Credentials(null, null));
+        public UVFMetadataPayload toPayload(final Path bucket, final SettingsDto settings) {
+            return this.toPayload(bucket, new Credentials(null, null), settings);
         }
 
         /**
          * Create UVF Metadata Payload
          *
          * @param credentials Static credentials for storage access
+         * @param settings Katta Server settings
          */
-        public UVFMetadataPayload toPayload(final Path bucket, final Credentials credentials) {
+        public UVFMetadataPayload toPayload(final Path bucket, final Credentials credentials, final SettingsDto settings) {
             return UVFMetadataPayload.create()
                     .withStorage(new VaultMetadataStorageDto()
                             .username(credentials.getUsername()).password(credentials.getPassword())
@@ -138,8 +140,8 @@ public class HubStorageLocationService implements Location {
                             .region(region)
                             .nickname(bucket.attributes().getDisplayname()))
                     .withAutomaticAccessGrant(new VaultMetadataAutomaticAccessGrantDto()
-                            .enabled(true)
-                            .maxWotDepth(-1));
+                            .enabled(settings.getEnableAutomaticAccessGrant())
+                            .maxWotDepth(settings.getAutomaticAccessGrantTrustThreshold()));
         }
     }
 }
--- hub/src/test/java/cloud/katta/workflows/AbstractHubWorkflowGroupTest.java
+++ hub/src/test/java/cloud/katta/workflows/AbstractHubWorkflowGroupTest.java
@@ -31,6 +31,7 @@ import cloud.katta.client.ApiClient;
 import cloud.katta.client.ApiException;
 import cloud.katta.client.JSON;
 import cloud.katta.client.api.GroupsResourceApi;
+import cloud.katta.client.api.SettingsResourceApi;
 import cloud.katta.client.api.StorageProfileResourceApi;
 import cloud.katta.client.api.UsersResourceApi;
 import cloud.katta.client.api.VaultResourceApi;
@@ -39,6 +40,7 @@ import cloud.katta.client.model.GroupDto;
 import cloud.katta.client.model.MemberDto;
 import cloud.katta.client.model.Role;
 import cloud.katta.client.model.S3StorageClass;
+import cloud.katta.client.model.SettingsDto;
 import cloud.katta.client.model.StorageProfileDto;
 import cloud.katta.client.model.StorageProfileS3STSDto;
 import cloud.katta.client.model.StorageProfileS3StaticDto;
@@ -73,6 +75,15 @@ abstract class AbstractHubWorkflowGroupTest extends AbstractHubTest {
             try (InputStream in = Objects.requireNonNull(this.getClass().getResourceAsStream(dockerConfig.envFile))) {
                 configuration.load(in);
             }
+
+            // enable automatic access grant, disable WoT verification
+            log.info("Enable automatic access grant and disable WoT");
+            final SettingsDto settings = new SettingsResourceApi(adminApiClient).apiSettingsGet();
+            settings.setEnableAutomaticAccessGrant(true);
+            settings.setAllowAutomaticAccessGrantOverride(true);
+            settings.setAutomaticAccessGrantTrustThreshold(-1);
+            settings.setAllowAutomaticAccessGrantOverride(true);
+            new SettingsResourceApi(adminApiClient).apiSettingsPut(settings);
 
             log.info("S00 admin uploads storage profile");
             final StorageProfileResourceApi adminStorageProfileApi = new StorageProfileResourceApi(adminApiClient);
