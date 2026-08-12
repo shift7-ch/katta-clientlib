be33f478fcf6fddef47ff31c93aad44322eb0741
David Kocher <dkocher@iterate.ch>
2026-08-12 09:50:44 +0200

    Generate toString implementation.

--- hub/src/main/java/cloud/katta/crypto/uvf/VaultMetadataAutomaticAccessGrantDto.java
+++ hub/src/main/java/cloud/katta/crypto/uvf/VaultMetadataAutomaticAccessGrantDto.java
@@ -112,24 +112,11 @@ public class VaultMetadataAutomaticAccessGrantDto {
 
     @Override
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("class AutomaticAccessGrant {\n");
-        sb.append("    enabled: ").append(toIndentedString(enabled)).append("\n");
-        sb.append("    maxWotDepth: ").append(toIndentedString(maxWotDepth)).append("\n");
-        sb.append("}");
+        final StringBuilder sb = new StringBuilder("VaultMetadataAutomaticAccessGrantDto{");
+        sb.append("enabled=").append(enabled);
+        sb.append(", maxWotDepth=").append(maxWotDepth);
+        sb.append('}');
         return sb.toString();
     }
-
-    /**
-     * Convert the given object to string with each line indented by 4 spaces
-     * (except the first line).
-     */
-    private String toIndentedString(Object o) {
-        if(o == null) {
-            return "null";
-        }
-        return o.toString().replace("\n", "\n    ");
-    }
-
 }
 
